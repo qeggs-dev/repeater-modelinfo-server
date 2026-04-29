e01132682db2ab7152a2ea87dad672037d544bde
Qeggs <get_boot@outlook.com>
2026-04-29 23:39:50 +0800

    refactor(global_config_manager): 移除ModelAPIConfig中的废弃配置项
    移除了ModelAPIConfig类中的case_sensitive配置项

--- info_server/global_config_manager/_models/_model_api/_model_api.py
+++ info_server/global_config_manager/_models/_model_api/_model_api.py
@@ -4,5 +4,4 @@ class ModelAPIConfig(BaseModel):
     model_config = ConfigDict(case_sensitive=False)
     
     api_file_path: str = "./configs/api_info.json"
-    case_sensitive: bool = False
     default_timeout: float = 600.0
